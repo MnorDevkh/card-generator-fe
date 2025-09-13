8bc0efbf8f2a08f8dd7952cae8383c02d8ba2eea
Boeurn Mnor <96278125+MnorDevkh@users.noreply.github.com>
2025-09-13 13:37:50 +0700

    fix

--- src/main.server.ts
+++ src/main.server.ts
@@ -5,3 +5,20 @@ import { config } from './app/app.config.server';
 const bootstrap = () => bootstrapApplication(App, config);
 
 export default bootstrap;
+
+export const prerender = {
+  routes: {
+    'card-generator/:id': getCardGeneratorParams,
+    'student-info/:id': getStudentInfoParams
+  }
+};
+
+function getCardGeneratorParams() {
+  // Replace with your actual IDs or fetch from DB
+  return [{ id: '1' }, { id: '2' }];
+}
+
+function getStudentInfoParams() {
+  // Replace with your actual IDs or fetch from DB
+  return [{ id: '1' }, { id: '2' }];
+}
