79c2e3f2bf870996588b684ec5ecee36d6d1985c
Mnor <bourenmno1111@gmail.com>
2025-10-02 16:26:50 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://cardsystemapi.aga-institute.edu.kh/',
-  url: 'https://cardsystemapi.aga-institute.edu.kh/'
+  url: 'https://hrm.aga-institute.edu.kh/'
 };
 
